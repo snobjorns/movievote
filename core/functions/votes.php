b139c00e3f94060b0efb1ea0f5322f698be22d96
Snorre <Snorrebj@student.matnat.uio.no>
2013-01-12 23:05:43 +0100

    votes are registered

--- core/functions/votes.php
+++ core/functions/votes.php
@@ -1,12 +1,12 @@
 <?php
  function register_votes($votes,$nightid,$uname){
+	
 	foreach($votes as $movieid => $vote){
 		global $user_data;
-		print_r($votes);
+		//print_r($votes);
 		$votevalue = $user_data['star'] * $vote; 
-		mysql_query("INSERT INTO votes (night_id,movie_id,voter,vote,votevalue) VALUES ($nightid $movieid $uname $vote $votevalue)");  
-		mysql_query("UPDATE movies SET (vote = votes + $vote , votevalue = votevalue + $votevalue) WHERE movieid = $movieid");
-	
+		mysql_query("INSERT INTO votes (night_id,movie_id,voter,vote,votevalue) VALUES ($nightid, $movieid, '$uname',$vote, $votevalue)");  
+		mysql_query("UPDATE movies SET votes = votes + $vote , votevalue = votevalue + $votevalue WHERE movieid = $movieid");
 	} 
  }
  

17a9480a1a20e6418647537b4825e9192de447a8
snobjorns <snorrebjornstad@gmail.com>
2013-01-17 17:08:56 +0100

    more test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $nighttime = datehour_to_time($data['nightdate'],$data['nighttime']);
 
 		
 <?php  
-echo check_if_voted(1,1);
+echo check_if_voted('snorrebj',1);
       echo "<h1>Next movienight has the theme <a href = poll.php?id=".$data['nightid'].">".$data['theme']."</a></h1>";
 	  
 	  echo "<p>Polls closes in ". round((datehour_to_time($data['nightdate'],"20:00:00")-24*60*60 - $time)/60/60,2) .  " hours<p>";	  
